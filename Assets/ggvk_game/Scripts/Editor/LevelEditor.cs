70cd82b744bc279a3d003e46e9257a00fbbaa747
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-10-22 16:09:01 +0300

    update

--- Assets/ggvk_game/Scripts/Editor/LevelEditor.cs
+++ Assets/ggvk_game/Scripts/Editor/LevelEditor.cs
@@ -8,14 +8,18 @@ public class LevelEditor : Editor
     Texture2D currentTexture;
     VisualElement myInspector;
 
+    LevelData levelData;
+
     public override VisualElement CreateInspectorGUI()
     {
+        levelData = target as LevelData;
+
         myInspector = new VisualElement();
         VisualTreeAsset visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/ggvk_game/Scripts/Editor/LevelInspector.uxml");
         visualTree.CloneTree(myInspector);
 
         Button middleBtn = myInspector.Query<Button>("btn_middle");
-        
+
         middleBtn.clickable.clicked += () =>
         {
             currentTexture = middleBtn.resolvedStyle.backgroundImage.texture;
@@ -36,9 +40,23 @@ public class LevelEditor : Editor
         var cells = myInspector.Query<Button>("cell");
         cells.ForEach((button) =>
         {
+            currentTexture = levelData.cells[0,0] switch
+            {
+                0 => middleBtn.resolvedStyle.backgroundImage.texture,
+                1 => angryBtn.resolvedStyle.backgroundImage.texture,
+                2 => funBtn.resolvedStyle.backgroundImage.texture,
+            };
+
+            StyleBackground styleBackground = new StyleBackground
+            {
+                value = new Background { texture = currentTexture }
+            };
+
+            button.style.backgroundImage = styleBackground;
+
             button.clickable.clicked += () =>
             {
-                if(!currentTexture)
+                if (!currentTexture)
                 {
                     return;
                 }
@@ -49,6 +67,22 @@ public class LevelEditor : Editor
                 };
 
                 button.style.backgroundImage = styleBackground;
+                
+                if(currentTexture.name == angryBtn.resolvedStyle.backgroundImage.texture.name)
+                {
+                    //levelData.cell0 = 1;
+                    levelData.cells[0, 0] = 1;
+                }
+                else if (currentTexture.name == middleBtn.resolvedStyle.backgroundImage.texture.name)
+                {
+                    //levelData.cell0 = 0;
+                    levelData.cells[0, 0] = 0;
+                }
+                else if (currentTexture.name == funBtn.resolvedStyle.backgroundImage.texture.name)
+                {
+                    //levelData.cell0 = 2;
+                    levelData.cells[0, 0] = 2;
+                }
             };
         });
 
--- Assets/ggvk_game/Scripts/LevelData.cs
+++ Assets/ggvk_game/Scripts/LevelData.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Level Editor/Create new Level Data", fileName = "New Level Data")]
+[CreateAssetMenu(menuName = "Level Editor/Create new Level Data")]
 public class LevelData : ScriptableObject
 {
-    
+    public int cell0;
+    public int[,] cells = new int[3, 3];
 }
